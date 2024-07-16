5b611fed93be0becc83a892c2fad95ac5ee7761d
Kaspesk <Kawasaki77k@outlook.com>
2024-07-16 01:33:30 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,8 @@ repo init -u https://github.com/crdroid-security/android.git -b 11.0 --git-lfs -
 # Clone local_manifests repository
 git clone https://github.com/kaspesk/local_manifests --depth 1 -b 11.0 .repo/local_manifests && 
 # Sync the repositories
-/opt/crave/resync.sh  && 
 # Set up build environment
 export BUILD_USERNAME=kaspesk 
 export BUILD_HOSTNAME=crave 
-source build/envsetup.sh && 
 # Build the ROM
-lunch lineage_starqltechn-userdebug && make installclean && mka bacon
+lunch lineage_channel-userdebug && make installclean && mka bacon
